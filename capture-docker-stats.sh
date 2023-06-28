73d525390dd0e182bcbf0fdd701c5e41df186b3b
Amir Iskander <amir.iskander@procore.com>
2023-06-29 02:18:31 +0300

    Adding a jmeter config file for load testing and a script to capture docker stats during the time jmeter tests are running

---
+++ capture-docker-stats.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set CSV file to log stats to  
+STATS_FILE=docker-stats.csv
+
+# Interval in seconds to capture stats
+INTERVAL=1
+
+# Check if trigger file exists
+while [ ! -f /tmp/triggerscript ]; do
+  sleep 1
+done
+
+# Header for CSV  
+echo -e "Name,CPU%,Memory Usage" > $STATS_FILE
+
+# Capture stats continuously 
+while true; do  
+  
+  STATS=$(docker stats --no-stream --format \
+           "{{.Name}},{{.CPUPerc}},{{.MemUsage}}")
+    
+  # Append to CSV
+  echo "$STATS" >> $STATS_FILE
+    
+  sleep $INTERVAL
+  
+done
