d7aced91be3b6e270d304d06845014cc3d45528b
Amir Iskander <amir.iskander@procore.com>
2023-07-03 03:52:22 +0300

    Adding the other endpoints testing memory usage, cpu usage and microbenchmarks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('microbenchmark')
+  microbenchmark(@Query('n') n): string {
+    return `Time taken: ${ this.appService.microbenchmarkTest(n) } ms`;
+  }
+  
+  @Get('memorytest')
+  memoryTest(@Query('n') n): string {
+    return `Memory usage: ${ this.appService.memoryTest(n) } MB`;
+  }
+
+  @Get('cputest')
+  cpuTest(@Query('n') n) : string {
+    return `Time taken: ${ this.appService.cpuTest(n) } ms`;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { performance } from 'perf_hooks';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  // Example of a microbenchmark for string concatenation in TypeScript (NestJS):
+  microbenchmarkTest(n: number): number {
+    const start = performance.now();
+    let result = '';
+    for (let i = 0; i < n; i++) {
+      result += i.toString();
+    }
+    const end = performance.now();
+    return end - start;
+  }
+
+  // Example of a memory usage test for a TypeScript (NestJS) server that generates a large amount of data
+  memoryTest(n: number): number {
+    const start = performance.now();
+    const data = [];
+    for (let i = 0; i < n; i++) {
+      data.push({ id: i, name: `User ${i}` });
+    }
+    return process.memoryUsage().heapUsed / 1024 / 1024;
+  }
+
+  // Example of a CPU usage test for a TypeScript (NestJS) server that performs a lot of calculations
+  cpuTest(n: number): number {
+    const start = performance.now();
+    let result = 0;
+    for (let i = 0; i < n; i++) {
+      result += i * i;
+    }
+    const end = performance.now();
+    return end - start;
   }
 }
